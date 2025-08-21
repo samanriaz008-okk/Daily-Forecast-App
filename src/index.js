7dfb9f65b9b73ae748097953150a7c35ce44c3a6
samanriaz008-okk <samanriaz008@gmail.com>
2025-08-21 16:35:46 +0500

    Added JS for displaying city name that is searched

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-
+function search(display) {
+  display.preventDefault();
+  let city = document.querySelector("h2");
+  let inputValue = document.querySelector("#search-input");
+  city.innerHTML = inputValue.value;
+}
+let button = document.querySelector("#search-button");
+button.addEventListener("click", search);
